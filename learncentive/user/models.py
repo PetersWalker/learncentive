503d31f03c4d8e3c59b204b80e14c56557ce8106
Peter Walker <petersinclairwalker@gmail.com>
2020-09-23 14:37:36 -0700

    User model tests passing on database

--- learncentive/user/models.py
+++ learncentive/user/models.py
@@ -3,13 +3,12 @@ from learncentive.app import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
+    name = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(120), unique=False, nullable=False)
-    grades = db.relationship('Grades', backref='user', lazy=True)
+    grades = db.Column(JSON)
 
 class Grades(db.Model):
      id = db.Column(db.Integer, primary_key=True)
-     subject = db.Column(db.String(80), unique=False, nullable=False)
-     grade_json = db.Column(db.String(80), unique=False)
+     grades = db.Column(JSON)
      user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
